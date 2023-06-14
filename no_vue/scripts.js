7e5d06c68faf9cb452db3526742d2f9e7588764b
codest <thecodest@ya.ru>
2023-06-14 19:16:21 +0300

    Feat script for receiving and handling data
    Now it's fully functional. Styling is the next part of the job.

--- no_vue/scripts.js
+++ no_vue/scripts.js
@@ -2,103 +2,258 @@ document.addEventListener('DOMContentLoaded', function() {
   const cardsApiLink = document.getElementById('cardsApiLink');
   const usersApiLink = document.getElementById('usersApiLink');
   
-  const filterCardsInput = document.getElementById('filterCardNumberInput');
-  const filterCardsApply = document.getElementById('filterCardNumberApply');
-  const filterCardsReset = document.getElementById('filterCardNumberReset');
-
+  // Получаем элемент контейнера секции, куда добавляем элемент контролов таблицы и самой таблицы
+  const sectionContainer = document.getElementById('section-container');
+  
+  const controlsContainer = document.createElement('div');
+  controlsContainer.setAttribute('class', 'table-controls section-container__table-controls');
+  
+  const tableContainer = document.createElement('div');
+  tableContainer.setAttribute('class', 'table-container section-container__table-container');
+  
+  // Объявим переменные для хранения готовых таблиц - оригинальной и сортированной. Пригодится для кеширования,
+  // чтобы не перезапрашивать и не перестраивать все заново при сбросе фильтрации или сортировки
+  let tableElement = null;
+  let usersSortedByName = null;
+  
   cardsApiLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    getDataFromApi('https://fakerapi.it/api/v1/credit_cards?_quantity=100', 'Credit cards data');
+    event.preventDefault();  /*Глушим дефолтное действие элемента*/
+    
+    // Очищаем все содержимое - и контролы, и саму таблицу
+    sectionContainer.innerHTML = '';
+    
+    
+    // Создаем элементы управления - кнопки, инпуты, вешаем атрибуты, потом добавляем все элементы в контейнер
+    const errorOutputBlock = document.createElement('p');
+    errorOutputBlock.setAttribute('class', 'error-message table-controls__error-message');
+    errorOutputBlock.hidden = true;
+    
+    const filterInputField = document.createElement('input');
+    filterInputField.setAttribute('class', 'input table-controls__input');
+    filterInputField.setAttribute('type', 'number');
+    filterInputField.setAttribute('placeholder', 'Номер карты или его часть');
+    
+    const filterApplyButton = document.createElement('button');
+    filterApplyButton.setAttribute('class', 'button table-controls__button');
+    filterApplyButton.innerText = 'Применить';
+    
+    const filterResetButton = document.createElement('button');
+    filterResetButton.setAttribute('class', 'button table-controls__button');
+    filterResetButton.innerText = 'Сбросить фильтр';
+    
+    
+    // Вешаем на контролы слушатели для запуска фильтрации или сброса фильтров
+    filterInputField.addEventListener('keydown', function (event) {
+      if (event.key === 'Enter') {
+        performFiltering(tableContainer, filterInputField, errorOutputBlock);
+      }
+    });
+    
+    filterApplyButton.addEventListener('click', function () {
+      performFiltering(tableContainer, filterInputField, errorOutputBlock);
+    });
+    
+    filterResetButton.addEventListener('click', function () {
+      if (tableElement) {
+        addTableToHtml(tableElement, tableContainer);
+        filterInputField.focus();
+        filterInputField.select();
+      }
+    })
+    
+    controlsContainer.innerHTML = '';
+    controlsContainer.append(errorOutputBlock, filterInputField, filterApplyButton, filterResetButton);
+    
+    
+    // Добавляем блок контролов и контейнер таблицы в основной контейнер
+    sectionContainer.append(controlsContainer, tableContainer);
+    
+    
+    // Работаем с контентом таблицы - получаем ответ от АПИ, строим таблицу, помещаем таблицу в контейнер
+    getDataFromApi('https://fakerapi.it/api/v1/credit_cards?_quantity=100')
+        .then(responseJson => buildTable(responseJson, 'Cards data table'))
+        .then(table => {
+          tableElement = table.cloneNode(true);
+          addTableToHtml(table, tableContainer);
+        });
   });
 
   usersApiLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    getDataFromApi('https://fakerapi.it/api/v1/users?_quantity=100', 'Users data');
-  });
-  
-  filterCardsApply.addEventListener('click', function () {
-    const filterCardsValue = filterCardsInput.value;
-    filterApply(filterCardsValue);
-  });
-  
-  filterCardsInput.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-      const filterValue = filterCardsInput.value;
-      filterApply(filterValue);
-    }
-  });
-  
-  filterCardsReset.addEventListener('click', function (event) {
-    filterCardsInput.value = '';
-    filterApply('');
+    event.preventDefault();  /*Глушим дефолтное действие элемента*/
+    
+    // Очищаем все содержимое - и контролы, и саму таблицу
+    sectionContainer.innerHTML = '';
+    
+    
+    // Создаем элементы управления - кнопки, инпуты, вешаем атрибуты, потом добавляем все элементы в контейнер
+    const sortApplyButton = document.createElement('button');
+    sortApplyButton.setAttribute('class', 'button table-controls__button');
+    sortApplyButton.innerText = 'Сортировать по имени';
+    
+    const sortResetButton = document.createElement('button');
+    sortResetButton.setAttribute('class', 'button table-controls__button');
+    sortResetButton.innerText = 'Сбросить сортировку';
+    
+    
+    // Вешаем слушатели для сортировки юзеров по имени и для сброса сортировки
+    sortApplyButton.addEventListener('click', function () {
+      if (usersSortedByName) {
+        addTableToHtml(usersSortedByName, tableContainer);
+      } else {
+        usersSortedByName = tableElement.cloneNode(true);
+        sortUsersByName(usersSortedByName);
+      }
+      addTableToHtml(usersSortedByName, tableContainer);
+    });
+    
+    sortResetButton.addEventListener('click', function () {
+      addTableToHtml(tableElement, tableContainer);
+    });
+    
+    controlsContainer.innerHTML = '';
+    controlsContainer.append(sortApplyButton, sortResetButton);
+    
+    
+    // Добавляем блок контролов и контейнер таблицы в основной контейнер
+    sectionContainer.append(controlsContainer, tableContainer);
+    
+    
+    // Работаем с контентом таблицы - получаем ответ от АПИ, строим таблицу, помещаем таблицу в контейнер
+    getDataFromApi('https://fakerapi.it/api/v1/users?_quantity=100')
+        .then(responseJson => buildTable(responseJson, 'Users data table'))
+        .then(table => {
+          tableElement = table.cloneNode(true);
+          usersSortedByName = null;
+          addTableToHtml(table, tableContainer);
+        });
   });
 });
 
-
-function getDataFromApi(url, tableName) {
-  fetch(url)
-    .then(response => response.json())
-    .then(responseData => {
-      if (responseData.status === 'OK') {
-        const tableContainer = document.getElementById('data-table__container');
-        tableContainer.innerHTML = '';
-        
-        const table = document.createElement('table');
-        table.setAttribute('class', 'cards-table');
-        
-        const tableCaption = document.createElement('caption');
-        tableCaption.setAttribute('class', 'cards-table__caption');
-        tableCaption.innerText = tableName
-        
-        const tableHeader = document.createElement('thead');
-        tableHeader.setAttribute('class', 'cards-table__header');
-        
-        const tableHeaderRow = document.createElement('tr');
-        for (const tableColumnName in responseData.data[0]) {
-          const tableData = document.createElement('td');
-          tableData.innerText = tableColumnName;
-          tableHeaderRow.appendChild(tableData);
+function getDataFromApi(apiUrl) {
+  return fetch(apiUrl)
+      .then(response => response.json())
+      .then(responseJsonData => {
+        if (responseJsonData.status === 'OK') {
+          return responseJsonData;
+        } else {
+          throw new Error('Не удалось получить данные по API');
         }
-        tableHeader.appendChild(tableHeaderRow);
-        
-        const tableBody = document.createElement('tbody');
-        tableBody.setAttribute('class', 'cards-table__body');
-        
-        responseData.data.forEach(cardData => {
-          const tableRow = document.createElement('tr');
-          const tableColumnValues = Object.values(cardData);
-          tableColumnValues.forEach(value => {
-            const tableData = document.createElement('td');
-            tableData.innerText = value;
-            tableRow.appendChild(tableData);
-          });
-          tableBody.appendChild(tableRow);
-        });
-        
-        table.appendChild(tableCaption);
-        table.appendChild(tableHeader);
-        table.appendChild(tableBody);
-        tableContainer.appendChild(table);
-      } else {
-        console.log('Ошибка при получении данных');
-      }
-    })
-    .catch(error => console.log('Ошибка при получении данных:', error));
+      })
+      .catch(error => console.log(error));
 }
 
-function filterApply(filterValue) {
-  const tableContainer = document.getElementById('data-table__container');
-  const table = tableContainer.querySelector('table');
-  const tableRows = table.querySelectorAll('tbody tr');
+function buildTable(jsonData, tableName) {
+  console.log(jsonData);
+  
+  const table = document.createElement('table');
+  table.setAttribute('class', 'table');
+  
+  const tableCaption = document.createElement('caption');
+  tableCaption.setAttribute('class', 'table-caption');
+  tableCaption.innerText = tableName;
+  
+  const tableHeader = document.createElement('thead');
+  tableHeader.setAttribute('class', 'table-header');
+  
+  const tableHeaderRow = document.createElement('tr');
+  for (const tableColumnName in jsonData.data[0]) {
+    const tableData = document.createElement('td');
+    tableData.innerText = tableColumnName;
+    tableHeaderRow.appendChild(tableData);
+  }
+  tableHeader.appendChild(tableHeaderRow);
+  
+  const tableBody = document.createElement('tbody');
+  tableBody.setAttribute('class', 'table-body');
+  
+  jsonData.data.forEach(cardData => {
+    const tableRow = document.createElement('tr');
+    const tableColumnValues = Object.values(cardData);
+    tableColumnValues.forEach(value => {
+      const tableData = document.createElement('td');
+      tableData.innerText = value;
+      tableRow.appendChild(tableData);
+    });
+    tableBody.appendChild(tableRow);
+  });
+  
+  table.appendChild(tableCaption);
+  table.appendChild(tableHeader);
+  table.appendChild(tableBody);
+  
+  return table;
+}
 
+function addTableToHtml(tableContent, htmlTableContainer) {
+  htmlTableContainer.innerHTML = '';
+  htmlTableContainer.appendChild(tableContent);
+}
+
+function performFiltering(tableContainer, filterValueInput, errorOutput) {
+  const filterValue = filterValueInput.value;
+  if (filterValue && validateInput(filterValue, errorOutput)) {
+    applyFiltering(tableContainer, filterValue);
+  } else {
+    filterValueInput.focus();
+    filterValueInput.select();
+  }
+}
+
+function validateInput(testValue, errorOutput) {
+  let isValid = true
+  
+  if (testValue.length === 1) {
+    errorOutput.innerText = 'Одна цифра?.. Чувак, ты серьезно?'
+    isValid = false;
+  } else {
+    if (!/^\d+$/.test(testValue)) {
+      errorOutput.innerText = 'Поиск номера карты возможен только по цифрам';
+      isValid = false;
+    }
+  }
+  
+  if (!isValid) {
+    errorOutput.removeAttribute('hidden');
+  } else {
+    errorOutput.innerText = '';
+    errorOutput.hidden = true;
+  }
+  
+  return isValid;
+}
+
+function applyFiltering(tableContainer, filterValue) {
+  const currentTable = tableContainer.querySelector('table');
+  const tableRows = currentTable.querySelectorAll('tbody tr');
+  
   tableRows.forEach(row => {
-    console.log(row)
     const cardNumber = row.cells[1].innerText;
-    console.log(cardNumber)
     if (!cardNumber.includes(filterValue)) {
       row.style.display = 'none';
     } else {
-      row.style.display = 'table-row'
+      row.style.display = 'table-row';
     }
+  });
+}
+
+function sortUsersByName(usersTable) {
+  const tableBody = usersTable.querySelector('tbody');
+  const tableRows = Array.from(tableBody.getElementsByTagName('tr'));
+  tableBody.innerHTML = '';
+  
+  tableRows.sort((userA, userB) => {
+    const firstNameA = userA.cells[2].innerText.toLowerCase();
+    const lastNameA = userA.cells[3].innerText.toLowerCase();
+    const fullNameA = `${firstNameA}${lastNameA}`;
+    
+    const firstNameB = userB.cells[2].innerText.toLowerCase();
+    const lastNameB = userB.cells[3].innerText.toLowerCase();
+    const fullNameB = `${firstNameB}${lastNameB}`;
+    
+    return fullNameA.localeCompare(fullNameB);
+  });
+  
+  tableRows.forEach(row => {
+    tableBody.appendChild(row);
   });
 }
